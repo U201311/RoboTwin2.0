5d297c574bf6384b4e464688aab585092961a1d3
u201311 <yingli950906@163.com>
2025-08-22 14:47:04 +0800

    modify entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,7 @@ EXPERT_DATA_NUM=$(get_json_value "['data_generation']['episode']")
 GPU_ID=$(get_json_value "['gpu_id']")
 SAVE_PATH=$(get_json_value "['data_generation']['output_path']")
 LOG_DIR=$(get_json_value "['data_generation']['log_path']")
+TASK_CONFIG=$(get_json_value "['data_generation']['task_config']")
 
 echo "Task Name: $TASK_NAME"
 echo "Camera Type: $CAMERA_TYPE"
@@ -21,7 +22,6 @@ mkdir -p "$LOG_DIR"
 DATE_STR=$(date +"%Y-%m-%d_%H-%M-%S")
 UUID=$(uuidgen)
 LOG_FILE="${LOG_DIR}/${TASK_NAME}_${CAMERA_TYPE}_${EXPERT_DATA_NUM}_${DATE_STR}_${UUID}.log"
-TASK_CONFIG=demo_randomized
 #source ~/miniconda3/bin/activate RoboTwin
 source /opt/conda/bin/activate RoboTwin
 
